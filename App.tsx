f3a5008f0a650ea6c3ad6273bb46d57c0fe22ad6
hamiltonspjr <hamilton-souza@outlook.com.br>
2025-05-21 14:55:44 -0300

    created textComponent and added font montserrat font to app

--- App.tsx
+++ App.tsx
@@ -1,16 +1,17 @@
 import {ThemeProvider} from '@shopify/restyle';
 import React from 'react';
 
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
 
 import {} from 'react-native/Libraries/NewAppScreen';
 import {theme} from './src/theme/theme';
+import {Text} from './src/components/Text/Text';
 
 function App(): React.JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <View>
-        <Text style={{color: theme.colors.red}}>teste</Text>
+        <Text preset="headingLarge">teste</Text>
       </View>
     </ThemeProvider>
   );
---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,74 @@
+import {createText} from '@shopify/restyle';
+import {Theme} from '../../theme/theme';
+import {TextStyle} from 'react-native';
+
+const RNText = createText<Theme>();
+type RNTextProps = React.ComponentProps<typeof RNText>;
+
+export interface TextProps extends RNTextProps {
+  preset?: TextVariants;
+  bold?: boolean;
+  semiBold?: boolean;
+  medium?: boolean;
+}
+
+export function Text({
+  children,
+  preset = 'paragraphSmall',
+  bold,
+  semiBold,
+  medium,
+  style,
+  ...rnTextProps
+}: TextProps) {
+  const fontFamily = getFontFamily(bold, semiBold, medium);
+  return (
+    <RNText
+      color="primaryContrast"
+      style={[$fontSizes[preset], {fontFamily}, style]}
+      {...rnTextProps}>
+      {children}
+    </RNText>
+  );
+}
+
+function getFontFamily(bold?: boolean, semiBold?: boolean, medium?: boolean) {
+  switch (true) {
+    case bold:
+      return $fontFamily.bold;
+    case semiBold:
+      return $fontFamily.semiBold;
+    case medium:
+      return $fontFamily.medium;
+    default:
+      return $fontFamily.regular;
+  }
+}
+
+type TextVariants =
+  | 'headingLarge'
+  | 'headingMedium'
+  | 'paragraphMedium'
+  | 'paragraphSmall';
+
+export const $fontSizes: Record<TextVariants, TextStyle> = {
+  headingLarge: {
+    fontSize: 37,
+  },
+  headingMedium: {
+    fontSize: 24,
+  },
+  paragraphMedium: {
+    fontSize: 16,
+  },
+  paragraphSmall: {
+    fontSize: 14,
+  },
+};
+
+export const $fontFamily = {
+  bold: 'Montserrat-Bold',
+  semiBold: 'Montserrat-SemiBold',
+  medium: 'Montserrat-Medium',
+  regular: 'Montserrat-Regular',
+};
